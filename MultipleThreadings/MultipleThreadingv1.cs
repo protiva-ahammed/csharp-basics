a25f837521e65059b7681466e3cfcd8149271b34
ahammed-protiva <protivaahammed7137@gmail.com>
2025-07-17 22:38:58 +0900

    initial  commit

---
+++ MultipleThreadings/MultipleThreadingv1.cs
@@ -0,0 +1,36 @@
+using System.IO.Pipes;
+using System.Reflection;
+
+namespace practices_basics.MultipleThreadings
+{
+
+    public class MultipleThreadingv1
+    {
+
+        public  void CountDesc()
+        {
+            for (int i = 10; i > 0; i--)
+            {
+                Console.WriteLine("from thread1 ", i);
+                Thread.Sleep(200);
+            }
+            Console.WriteLine("from thread1 completed . ");
+
+        }
+        
+         public  void CountAsc()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("from thread2 ", i);
+                                Thread.Sleep(100);
+
+            }
+            
+                                        Console.WriteLine("from thread2 completed . ");
+
+        }
+
+}
+    
+}
---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+using System.IO.Pipes;
+using System.Reflection;
+using practices_basics.MultipleThreadings;
+
+namespace practices_basics
+{
+    class Program
+    {
+        // Thread is AnonymousPipeClientStream execution path of a program
+        // MulticastDelegate-thread to perform diff task at same time concurrently
+        // currently running thread is main thread  whic is int System.Threading 
+        static void Main(string[] args)
+        {
+
+            Thread mainThread = Thread.CurrentThread;
+            mainThread.Name = " main thread ";
+            Console.WriteLine(mainThread.Name);
+            MultipleThreadingv1 obj = new MultipleThreadingv1();
+            Thread thread1 = new Thread(obj.CountDesc);
+
+                        Thread thread2 = new Thread(obj.CountAsc);
+            thread1.Start();
+            thread2.Start();
+
+                                        Console.WriteLine("from main thread completed . ");
+
+            Console.ReadKey();
+
+        }
+
+      
+
+       
+       
+
+
+    }
+}
