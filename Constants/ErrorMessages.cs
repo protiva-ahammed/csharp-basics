6309d102733e166bd0d825181c07d038ddd0c0e9
ahammed-protiva <protivaahammed7137@gmail.com>
2025-07-22 22:24:39 +0900

    use  of constants

---
+++ Constants/ErrorMessages.cs
@@ -0,0 +1,11 @@
+using System.Reflection.Emit;
+
+namespace practices_basics.Constants
+{
+    public class ErrorMessages
+    {
+        public const string INVALID_LOGIN = "Invalid User!";
+        public const string NEGATIVE_NUMBER = "Enter a positive number :(";
+    }
+
+}
